c26e24366bcdb41a68ee75f364e270554b96422e
omercaylan <omerceylan38@gmail.com>
2020-06-24 14:37:41 +0300

    orders added

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,4 +1,42 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/')
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling GET request  to / product'
+    });
+});
+
+router.post('/', (req, res, next) => {
+    res.status(201).json({
+        message: 'Handling POST request  to / product'
+    });
+});
+
+router.get('/:productID', (req, res, next) => {
+    const id = req.params.productID;
+    if (id == 'special') {
+        res.status(200).json({
+            message: 'You discover the special ID ',
+            id: id
+        });
+    } else {
+        res.status(200).json({
+            message: 'You passed an id'
+        });
+    }
+});
+
+router.patch('/:productID', (req, res, next) => {
+    res.status(200).json({
+        message: 'Update Product'
+    });
+});
+
+router.delete('/:productID', (req, res, next) => {
+    res.status(200).json({
+        message: 'Delete Product'
+    });
+});
+
+module.exports = router;
--- app.js
+++ app.js
@@ -2,13 +2,10 @@ const express = require('express');
 const app = express(); //constructor
 
 const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
 
 //burada lamda kullandik fakat farkli bir fonksiyon yazip onu burada verebilirdik
-app.use((req,res,next) =>{
-    res.status(200).json({
-        message : 'it works'
-    });
-
-});
+app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
 
 module.exports = app;
