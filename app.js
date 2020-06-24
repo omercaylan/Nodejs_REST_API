f7ee51c056e9b9d5c83218ee99a9a05755b064d0
omercaylan <omerceylan38@gmail.com>
2020-06-24 18:18:57 +0300

    error handling default logging added

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const express = require('express');
+const morgan = require('morgan');
+
 const app = express(); //constructor
+
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
-//burada lamda kullandik fakat farkli bir fonksiyon yazip onu burada verebilirdik
+app.use(morgan('dev'));
+//burada lamda kullandik fakat farkli bir fonksiyon yazip onu burada verebilirdik handle reques
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;
